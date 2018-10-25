eae6b185869d2c46717481228f422f220e1d2561
jordan-verint <jordan.watson@verint.com>
2018-10-25 10:08:30 +0100

    Added jordans working code

---
+++ 1.Fibonacci-Sequence/Jordan/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace fibo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program fib = new Program();
+            // int number = Console.ReadLine();
+            int number = Convert.ToInt32(Console.ReadLine());
+            fib.Fibo(number);
+        }
+
+        public int Fibo(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            Console.WriteLine(a);
+            return a;
+        }
+    }
+}
